2fee7cd96ceb3846ba5dcd4c272757b30015ca99
Tim Ubial <tubial@sd38.bc.ca>
2021-10-14 11:38:57 -0700

    Refactored greyscale functions

--- image_magic.py
+++ image_magic.py
@@ -4,6 +4,46 @@
 from PIL import Image
 
 
+def to_greyscale(pixel: tuple) -> tuple:
+    """Convert a pixel to greyscale.
+
+    Args:
+        pixel: a 3-tuple of ints from
+            0-255, e.g. (140, 120, 255)
+            represents (red, green, blue)
+
+    Returns:
+        a 3-tuple pixel (r, g, b) in
+        greyscale
+    """
+    # grab r, g, b
+    red, green, blue = pixel
+
+    # calculate the average
+    average = int((red + green + blue) / 3)
+
+    return average, average, average
+
+
+def to_greyscale_luma(pixel: tuple) -> tuple:
+    """Convert to greyscale using luma algorithm
+
+    Args:
+        pixel: a 3-tuple of ints from
+            0-255, e.g. (140, 120, 255)
+            represents (red, green, blue)
+
+    Returns:
+        a 3-tuple pixel (r, g, b) in
+        greyscale
+    """
+    red, green, blue = pixel
+
+    grey = int(red * 0.3 + green * 0.59 + blue * 0.11)
+
+    return grey, grey, grey
+
+
 # Load the image (pumpkin)
 # Open an output image that's the same size
 image = Image.open('./halloween-unsplash.jpg')
@@ -31,16 +71,9 @@ for y in range(image_height):
         # Grab pixel information for THIS pixel
         pixel = image.getpixel((x, y))
 
-        # grab r, g, b
-        red, green, blue = pixel
-
-        # calculate the average
-        average = int((red + green + blue) / 3)
-
-        # create a gray pixel
-        gray_pixel = (average, average, average)
+        grey_pixel = to_greyscale_luma(pixel)
 
         # put that in the new image
-        output_image.putpixel((x, y), gray_pixel)
+        output_image.putpixel((x, y), grey_pixel)
 
-output_image.save('grayscale.jpg')
+output_image.save('grayscale_luma.jpg')
