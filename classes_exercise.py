da13c2ad0b1a8396ff69ec940fb356285ab66c64
Tim Ubial <tubial@sd38.bc.ca>
2021-10-28 10:47:16 -0700

    Implemented Bus and the fare() method

--- classes_exercise.py
+++ classes_exercise.py
@@ -46,9 +46,34 @@ class Vehicle:
         print("Vroom" * self.max_speed)
 
 
+class Bus(Vehicle):
+    """Bus is a Vehicle that can drive
+    humans around in it"""
+    def fare(self, age: int) -> None:
+        """Tells how much fare is for a particular age"""
+        if 18 <= age <= 60:
+            print("The fare of this bus ride is $5.00. 🚌")
+        else:
+            print("You ride free!~ 🚌")
+
+
 a_vehicle = Vehicle()
 a_vehicle.name = "La Ferrari"
 a_vehicle.max_speed = 372
 a_vehicle.capacity = 2
 a_vehicle.vroom()
+
+a_bus = Bus()
+a_bus.name = "Tranlink Bus - 407"
+a_bus.capacity = 35
+a_bus.max_speed = 140
+a_bus.vroom()
+a_bus.fare(10)
+print()
+a_bus.fare(-1)
+a_bus.fare(0)
+a_bus.fare(17)
+a_bus.fare(18)
+a_bus.fare(60)
+a_bus.fare(61)
 
