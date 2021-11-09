c99c6f1664a26ac5fe65a22ed4f4279f5575ef35
Tim Ubial <tubial@sd38.bc.ca>
2021-11-09 08:47:19 -0800

    Added pygame_drawing and completed some boilerplate

---
+++ pygame_drawing.py
@@ -0,0 +1,26 @@
+# Pygame_Drawing
+# Author: Ubial
+# 9 November 2021
+
+# Get introduced to Pygame and draw objects on screen
+
+import pygame
+
+pygame.init()
+
+WHITE = (255, 255, 255)
+BLACK = (  0,   0,   0)
+RED   = (255,   0,   0)
+GREEN = (  0, 255,   0)
+BLUE  = (  0,   0, 255)
+
+SCREEN_WIDTH  = 800
+SCREEN_HEIGHT = 600
+SCREEN_SIZE   = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+def main() -> None:
+    """Driver of the Python script"""
+
+
+if __name__ == "__main__":
+    main()
